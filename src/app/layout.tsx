d70b27cbf1b07b695f12fd5e5cd64eeb2164e9d9
mukies <muk.yess@gmail.com>
2025-07-06 11:51:05 +0545

    GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,8 +74,27 @@ export default function RootLayout({
         {/* ✅ Favicon manually declared here */}
         <meta name="publisher" content="Mukesh Bhattarai" />
         <meta name="robots" content="index, follow" />
+         <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K4RCTD55');
+            `,
+          }}
+        />
       </head>
       <body className={`${inter.className} transition-colors duration-300`}>
+         <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K4RCTD55"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <ThemeProvider>
           {/* <CustomCursor /> */}
           <Header />
